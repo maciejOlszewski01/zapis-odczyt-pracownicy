285c4fc3b7724792509a35f19862fbadad4fc39e
maciejOlszewski01 <66844898+maciejOlszewski01@users.noreply.github.com>
2022-01-26 12:20:57 +0100

    ff
    f

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,12 +7,16 @@ import java.util.zip.*;
 
 import model.*;
 import controller.Metody;
-public class Main {
 
+import static controller.Metody.odczyt;
+import static controller.Metody.zapis;
+
+public class Main {
+    public static HashMap<String, Pracownik> Lista = new HashMap<String, Pracownik>();
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         Scanner kbreader = new Scanner(System.in);
-        HashMap<String, Pracownik> Lista = new HashMap<String, Pracownik>();
+
         while(true){
             System.out.print("\nMenu\n" +
                     "1.Lista pracowników\n" +
@@ -52,6 +56,7 @@ public class Main {
                         break;
                     }catch(NullPointerException e){
                         System.out.print("Pracownik o podanym Peselu nie został odnaleziony\n");
+                        break;
                     }
                 case 4:
                     System.out.print("[Z]achowaj/[O]dtworz\t:\t");
@@ -60,79 +65,13 @@ public class Main {
                     String roz;
                     System.out.print("-------------------------------\n");
                     if(c.equals("Z")){
-                        System.out.print("Kompresja [G]zip/[Z]ip\t:\t");
-                        roz = kbreader.next();
-                        System.out.print("Nazwa pliku\t:\t");
-                        plik = kbreader.next();
-                        System.out.print("-------------------------------\n");
-                        System.out.print("[G]-Potwierdź\n");
-                        System.out.print("[Q]-Powrzuć\n");
-                        if(roz.equals("G")) {
-                            if (kbreader.next().equals("G")) {
-                                FileOutputStream fos = new FileOutputStream(plik);
-                                GZIPOutputStream gz = new GZIPOutputStream(fos);
-                                ObjectOutputStream oos = new ObjectOutputStream(gz);
-                                oos.writeObject(Lista);
-                                oos.close();
-                            } else {
-                                break;
-                            }
-                        } else{
-                            if (kbreader.next().equals("G")) {
-                                FileOutputStream fos = new FileOutputStream("temp.ser");
-                                ObjectOutputStream oos = new ObjectOutputStream(fos);
-                                oos.writeObject(Lista);
-                                oos.close();
-                                int iByte = 0;
-                                FileOutputStream f = new FileOutputStream(plik);
-                                ZipOutputStream zos = new ZipOutputStream(f);
-                                BufferedOutputStream out = new BufferedOutputStream(zos);
-                                BufferedInputStream in = new BufferedInputStream(new FileInputStream("temp.ser"));
-                                zos.putNextEntry (new ZipEntry ("temp.ser"));
-                                while ((iByte = in.read()) != -1 ){
-                                    out.write (iByte);
-                                };
-                                in.close();out.flush();out.close();
-                            } else {
-                                break;
-                            }
-                        }
+
+                        //ZAPIS KURWAAAAA
+                    zapis();
+                    break;
+
                     } else if(c.equals("O")){
-                        System.out.print("Nazwa pliku\t:\t");
-                        plik = kbreader.next();
-                        System.out.print("-------------------------------\n");
-                        System.out.print("[G]-Potwierdź\n");
-                        System.out.print("[Q]-Powrzuć\n");
-                        if(kbreader.next().equals("G")){
-                            if(plik.charAt(plik.length()-2) == 'g') {
-                                FileInputStream fi = new FileInputStream(plik);
-                                GZIPInputStream in = new GZIPInputStream(fi);
-                                ObjectInputStream ois = new ObjectInputStream(in);
-                                Lista = (HashMap<String, Pracownik>) ois.readObject();
-                                ois.close();
-
-                                continue;
-                            }else{
-                                FileInputStream fi = new FileInputStream(plik);
-                                int iByte = 0;
-                                FileOutputStream o2 = new FileOutputStream("temp2.ser");
-                                ZipInputStream in2 = new ZipInputStream(fi);
-                                BufferedInputStream bis = new BufferedInputStream(in2);
-                                while(in2.getNextEntry()!=null){
-                                    while((iByte = bis.read()) != -1){
-                                        o2.write(iByte);
-                                    }
-                                }
-                                o2.close();
-                                FileInputStream tr = new FileInputStream("temp2.ser");
-                                ObjectInputStream trn = new ObjectInputStream(tr);
-                                Lista = (HashMap<String, Pracownik>) trn.readObject();
-                                trn.close();
-                            }
-                        }else{
-                            break;
-                        }
-
+                        odczyt();
                     }
                     break;
                 default:
--- src/controller/Metody.java
+++ src/controller/Metody.java
@@ -1,9 +1,19 @@
 package controller;
 import model.*;
 
+import java.io.*;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.zip.*;
+
+import static com.company.Main.Lista;
 
 public class Metody<J> {
 
@@ -122,5 +132,221 @@ public class Metody<J> {
             System.out.print("Zła długość peselu\nPopraw dane\n");
             return false;
         }
+    }
+    static void zapis_pojZip(String plik, Pracownik a, String b) throws IOException {
+        String temp = "temp";
+        temp += b;
+        temp += ".ser";
+        plik +=b;
+        plik +=".zip";
+        FileOutputStream fos = new FileOutputStream(temp);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(a);
+        oos.close();
+        int iByte = 0;
+        FileOutputStream f = new FileOutputStream(plik);
+        ZipOutputStream zos = new ZipOutputStream(f);
+        BufferedOutputStream out = new BufferedOutputStream(zos);
+        BufferedInputStream in = new BufferedInputStream(new FileInputStream(temp));
+        zos.putNextEntry (new ZipEntry(temp));
+        while ((iByte = in.read()) != -1 ){
+            out.write (iByte);
+        };
+        in.close();out.flush();out.close();
+    }
+    static void zapis_poj(String plik,Pracownik a, String b) throws IOException {
+
+        plik +=b;
+        plik +=".gz";
+        FileOutputStream fos = new FileOutputStream(plik);
+        GZIPOutputStream gz = new GZIPOutputStream(fos);
+        ObjectOutputStream oos = new ObjectOutputStream(gz);
+        oos.writeObject(a);
+        oos.close();
+    }
+    public static void zapis() throws IOException {
+        String plik;
+        Scanner kbreader = new Scanner(System.in);
+        System.out.print("Kompresja [G]zip/[Z]ip\t:\t");
+        String roz = kbreader.next();
+        System.out.print("Nazwa pliku\t:\t");
+        plik = kbreader.next();
+        System.out.print("-------------------------------\n");
+        System.out.print("[G]-Potwierdź\n");
+        System.out.print("[Q]-Powrzuć\n");
+        if(roz.equals("G")) {
+
+            if (kbreader.next().equals("G")) {
+                int counter = 1;
+                ExecutorService executor = Executors.newFixedThreadPool(10);
+                List<CompletableFuture<Void>> completableFutureList = new ArrayList<>(Lista.size());
+                for (String key: Lista.keySet()) {
+                    String tem = Integer.toString(counter);
+
+                    completableFutureList.add(CompletableFuture.runAsync(() -> {
+                        try {
+                            zapis_poj(plik, Lista.get(key), tem);
+                        } catch (IOException e) {
+                            throw new CompletionException(e);
+                        }
+                    } , executor));
+                    counter++;
+                }
+                completableFutureList.forEach((a)->a.join());
+
+            }
+        } else{
+            if (kbreader.next().equals("G")) {
+                int counter = 1;
+                ExecutorService executor = Executors.newFixedThreadPool(10);
+                List<CompletableFuture<Void>> completableFutureList = new ArrayList<>(Lista.size());
+                for (String key: Lista.keySet()) {
+                    String tem = Integer.toString(counter);
+
+                    completableFutureList.add(CompletableFuture.runAsync(() -> {
+                        try {
+                            zapis_pojZip(plik, Lista.get(key), tem);
+                        } catch (IOException e) {
+                            throw new CompletionException(e);
+                        }
+                    } , executor));
+                    counter++;
+                }
+                completableFutureList.forEach((a)->a.join());
+            }
+    }}
+
+     static int counter_g(String a) throws FileNotFoundException {
+
+
+        int b = 0;
+        int c = 1;
+        String temp ="";
+        try{
+        while(true) {
+            temp = a;
+            temp += Integer.toString(c);
+            temp +=".gz";
+            FileInputStream fi = new FileInputStream(temp);
+            b++;
+            c++;
+        }}catch(FileNotFoundException g){
+            return b;
+        }
+
+     }
+
+    static int counter_z(String a) throws FileNotFoundException {
+
+
+        int b = 0;
+        int c = 1;
+        String temp;
+        try{
+            while(true) {
+                temp = a;
+                temp += Integer.toString(c);
+                temp +=".zip";
+                FileInputStream fi = new FileInputStream(temp);
+                b++;
+                c++;
+            }}catch(FileNotFoundException g){
+            return b;
+        }
+
+    }
+
+    public static Pracownik odczyt_p(String plik, String a) throws IOException, ClassNotFoundException {
+        plik += a;
+        plik += ".gz";
+        FileInputStream fi = new FileInputStream(plik);
+        GZIPInputStream in = new GZIPInputStream(fi);
+        ObjectInputStream ois = new ObjectInputStream(in);
+        Pracownik temp = (Pracownik) ois.readObject();
+        ois.close();
+        return temp;
+    }
+
+    public static Pracownik odczyt_pz(String plik,String a) throws IOException, ClassNotFoundException {
+        plik += a;
+        plik += ".zip";
+        FileInputStream fi = new FileInputStream(plik);
+        int iByte = 0;
+        FileOutputStream o2 = new FileOutputStream("temp2.ser");
+        ZipInputStream in2 = new ZipInputStream(fi);
+        BufferedInputStream bis = new BufferedInputStream(in2);
+        while(in2.getNextEntry()!=null){
+            while((iByte = bis.read()) != -1){
+                o2.write(iByte);
+            }
+        }
+        o2.close();
+        FileInputStream tr = new FileInputStream("temp2.ser");
+        ObjectInputStream trn = new ObjectInputStream(tr);
+        Pracownik temp = (Pracownik) trn.readObject();
+        trn.close();
+        return temp;
+    }
+
+    public static Pracownik odczyt() throws IOException, ClassNotFoundException {
+        String plik;
+        Scanner kbreader = new Scanner(System.in);
+        System.out.print("Nazwa pliku\t:\t");
+        plik = kbreader.next();
+        System.out.print("-------------------------------\n");
+        System.out.print("[G]-Potwierdź\n");
+        System.out.print("[Q]-Powrzuć\n");
+        if(kbreader.next().equals("G")){
+            if(plik.charAt(plik.length()-2) == 'g') {
+                System.out.print(counter_g(plik));
+                ExecutorService executor = Executors.newFixedThreadPool(10);
+                List<CompletableFuture<Pracownik>> completableFutureList = new ArrayList<>(Lista.size());
+                for(int i = 0; i < counter_g(plik);i++){
+                    int temp2 = i+1;
+                    String temp3 = Integer.toString(temp2);
+                    completableFutureList.add(CompletableFuture.supplyAsync(() -> {
+                        try {
+                            Pracownik temp = odczyt_p(plik, temp3);
+                            Lista.put(temp.getPesel(),temp);
+                            return temp;
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (ClassNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                        return null;
+                    },executor));
+
+                }
+                completableFutureList.forEach((a)->a.join());
+            }else {
+
+                for(int i = 0; i < counter_z(plik);i++){
+                    int temp2 = i+1;
+
+                    String temp3 = Integer.toString(temp2);
+
+                    ExecutorService executor = Executors.newFixedThreadPool(10);
+                    List<CompletableFuture<Pracownik>> completableFutureList = new ArrayList<>(Lista.size());
+                    completableFutureList.add(CompletableFuture.supplyAsync(() -> {
+                        Pracownik temp = null;
+                        try {
+                            temp = odczyt_pz(plik, temp3);
+                            Lista.put(temp.getPesel(), temp);
+                            return temp;
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (ClassNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                        return temp;
+                    },executor));
+                    completableFutureList.forEach((a)->a.join());
+                }
+
+            }
+        }
+        String p = "01251006218";
+        return Lista.get(p);
     }
 }
